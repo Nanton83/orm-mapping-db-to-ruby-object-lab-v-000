cbd6bcba00ae7acb6c60f4904a1dfecede68a1e8
Nick Romans <nicholas.romans@gmail.com>
2019-01-04 19:34:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,6 +79,16 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+    SELECT * 
+    FROM students 
+    WHERE grade = ?
+    SQL
+    
+    DB[:conn].execute(sql, x)
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
